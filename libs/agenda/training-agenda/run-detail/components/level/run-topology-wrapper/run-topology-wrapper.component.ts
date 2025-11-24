1591129359f6ef55790e281f2f0394364efc4645
Mário Murín <murin@cshub.cz>
2025-11-24 15:05:40 +0100

    Improve topology logic, add bounds, reformat, make levels display correctly

--- libs/agenda/training-agenda/run-detail/components/level/run-topology-wrapper/run-topology-wrapper.component.ts
+++ libs/agenda/training-agenda/run-detail/components/level/run-topology-wrapper/run-topology-wrapper.component.ts
@@ -2,37 +2,44 @@ import {
     Component,
     ElementRef,
     inject,
+    input,
     OnDestroy,
     OnInit,
     Renderer2,
     signal,
 } from '@angular/core';
 import { AbstractTrainingRunService } from '../../../services/training-run/abstract-training-run.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgClass } from '@angular/common';
 import {
     TopologySynchronizerService,
     TopologyWrapperComponent,
 } from '@crczp/topology-graph';
 import { SshAccessService } from '../../../services/training-run/ssh/ssh-acess.service';
 import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
     selector: 'crczp-run-topology-wrapper',
-    imports: [AsyncPipe, TopologyWrapperComponent, MatIcon],
+    imports: [
+        AsyncPipe,
+        TopologyWrapperComponent,
+        MatIcon,
+        NgClass,
+        MatTooltip,
+    ],
     templateUrl: './run-topology-wrapper.component.html',
     styleUrl: './run-topology-wrapper.component.scss',
 })
 export class RunTopologyWrapperComponent implements OnInit, OnDestroy {
+    topologyAllowed = input<boolean>(true);
     protected readonly runService = inject(AbstractTrainingRunService);
     protected readonly sshAccessService = inject(SshAccessService);
     protected readonly topologyService = inject(TopologySynchronizerService);
     protected readonly collapsed = signal<boolean>(false);
-
     private readonly renderer = inject(Renderer2);
     private readonly elementRef = inject(ElementRef);
-
     private ancestorPaddedContent: HTMLElement | null = null;
-    private originalPaddingRight: string = '';
+    private originalPaddingRight = '';
 
     constructor() {
         this.topologyService.topologyCollapsed$.subscribe((collapsed) =>
