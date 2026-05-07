60398d223d507c38fae8ca9cbb037c204844826b
Mário Murín <murin@cshub.cz>
2026-05-07 14:13:23 +0200

    Add test utils

--- libs/utils/misc/src/index.ts
+++ libs/utils/misc/src/index.ts
@@ -24,6 +24,7 @@ export * from './service/progress/file-upload-progress.service';
 export * from './service/api/polling.service';
 export * from './types/config';
 export * from './types/sentinel-auth-config.zod';
+export * from './types/type-utils';
 export * from './directives/click-outside';
 export type {
     NavAgendaConfig,
